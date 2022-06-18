620479abcb9da6a144bc0c090c12d71b8a49c768
myaosato <tetu60u@yahoo.co.jp>
2022-06-18 15:40:48 +0900

    fix example specialize

--- src/examples/first-order-logic.lisp
+++ src/examples/first-order-logic.lisp
@@ -8,8 +8,8 @@
 (def-predicate p 1)
 
 (def-theorem specialization (→ (∀ x (p x)) (∃ x (p x)))
-  (:vars (x y))
+  (:vars (x))
   (to-r 0)
-  (forall-l 0 y)
-  (exists-r 0 y)
+  (forall-l 0 x)
+  (exists-r 0 x)
   (id 0))
